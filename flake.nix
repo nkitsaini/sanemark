71dccb6164bd4ae6664fb2b1b9a1832ae415add6
Ankit Saini <nnkitsaini@gmail.com>
2026-09-05 09:34:52 +0530

    chore: release v0.1.1

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
         {
           default = pkgs.rustPlatform.buildRustPackage {
             pname = "sanemark";
-            version = "0.1.0";
+            version = "0.1.1";
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
 
